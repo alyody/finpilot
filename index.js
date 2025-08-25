22f66253b982885c47debf94f9121f06355f2d79
alyody <alistar.concessio1@gmail.com>
2025-08-25 23:50:26 +0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { signup, login } from '../controllers/user.controller.js/controller.js';
-
-const router = express.Router();
-
-router.post('/user/signup', signup);
-router.post('/user/login', login);
-
-export default router;
