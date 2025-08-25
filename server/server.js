8195e742c4f350df3bdb4d05e9c726eeabc76b5a
alyody <alistar.concessio1@gmail.com>
2025-08-26 00:04:03 +0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client';
-import routes from './routes/index.js';
+import routes from './index.js';        // ← Changed this line
 const prisma = new PrismaClient();
 
 dotenv.config({ path: './.env' });
